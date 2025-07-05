54eb1d225c80a40cabb91ee4956909c01bb9cfcb
FranCaporale <caporalefranco@icloud.com>
2025-07-05 23:58:23 +0100

    Update Servidor.js

--- Servidor.js
+++ Servidor.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 const session = require("express-session");
 const multer = require('multer');
 const { error } = require('console');
